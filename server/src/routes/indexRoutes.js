fecb65da2ad1b00b96090ec13d0d8baed512d68a
AlekseyAleksandrov <axcerz@gmail.com>
2025-04-18 23:50:06 +0300

    create: Reg, Auth, Logut, Refresh routes and logics for server side

--- server/src/routes/indexRoutes.js
+++ server/src/routes/indexRoutes.js
@@ -1,3 +1,18 @@
 const router = require("express").Router();
+const {
+  registration,
+  login,
+  logout,
+  refresh,
+} = require("../controllers/userController");
+
+const authMiddleware = require("../middlewares/authMiddleware");
+
+router.post("/registration", registration);
+router.post("/login", login);
+router.post("/logout", logout);
+router.get("/refresh", refresh);
+
+// router.use('/themes', )
 
 module.exports = router;
