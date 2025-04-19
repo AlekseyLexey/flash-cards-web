ab0f225032622a2e7f0fa1faa0ae76bc58c52543
AlekseyAleksandrov <axcerz@gmail.com>
2025-04-19 17:05:15 +0300

    add: logic for quiz

--- client/src/pages/QuestionPage.jsx
+++ client/src/pages/QuestionPage.jsx
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import quizQuestions from "./quizQuestions";
 import $api from "../http/axiosConfig";
 
-function QuestionPage() {
+const POINT = 5;
+
+function QuestionPage({ user_id = 1 }) {
   const [questions, setQuestions] = useState(null);
   const [themeInfo, setThemeInfo] = useState(null);
   const [answer, setAnswer] = useState("");
+  const [questionCounter, setQuestionCounter] = useState(0);
+  const [result, setResult] = useState({
+    user_id,
+    total_points: 0,
+    first_time: 0,
+    theme_id: null,
+  });
+  const [isDone, setDone] = useState(false);
+  const [hasPostedPoints, setHasPostedPoints] = useState(false);
 
   const { id } = useParams();
   const navigate = useNavigate();
@@ -15,7 +25,10 @@ function QuestionPage() {
     async function fetchingQuestions() {
       try {
         const { data } = await $api.get(`/themes/${id}`);
-        setThemeInfo({ theme_id: data.id, name: data.theme });
+        setResult((prev) => {
+          return { ...prev, theme_id: data.id };
+        });
+        setThemeInfo({ name: data.theme });
         setQuestions(data.theme_question);
       } catch (error) {
         console.log(error);
@@ -24,25 +37,45 @@ function QuestionPage() {
     fetchingQuestions();
   }, []);
 
-  console.log(questions);
-  console.log(themeInfo);
+  useEffect(() => {
+    async function fetchingPoints() {
+      try {
+        if (isDone && !hasPostedPoints) {
+          await $api.post(`/point`, result);
+          setHasPostedPoints(true);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchingPoints();
+  }, [isDone]);
 
-  const handleClear = () => {
+  const handleCheckAnswer = () => {
+    const currentQuestion = questions[questionCounter].answer;
+
+    if (currentQuestion.toLowerCase() == answer.toLowerCase()) {
+      setResult((prev) => {
+        return { ...prev, total_points: prev.total_points + POINT };
+      });
+    }
+    setQuestionCounter((prevCounter) => prevCounter + 1);
     setAnswer("");
+    if (questionCounter + 1 === questions.length) {
+      setDone(true);
+    }
   };
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      setAnswer("");
+      handleCheckAnswer();
     }
   };
 
-  const question = quizQuestions.find((el) => el.id === +id);
-
-  return (
-    <div>
+  const content = !isDone ? (
+    <>
       <h2>Тема: {themeInfo?.name}</h2>
-      <div>{question.question}</div>
+      <div>{questions ? questions[questionCounter].question : "Загрузка"}</div>
       <input
         type="text"
         className="input"
@@ -50,9 +83,17 @@ function QuestionPage() {
         onChange={(e) => setAnswer(e.target.value)}
         onKeyDown={handleKeyDown}
       />
-      <button onClick={handleClear}>Send</button>
+      <button onClick={handleCheckAnswer}>Send</button>
+    </>
+  ) : (
+    <>Вы прошли QUIZ ваш счет: ${result.total_points}</>
+  );
+
+  return (
+    <div>
+      {content}
       <div>
-        <button onClick={() => navigate(-1)}>Main page</button>
+        <button onClick={() => navigate("/quiz")}>Main page</button>
       </div>
     </div>
   );
