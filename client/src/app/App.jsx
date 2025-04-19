dbb2a064f42b336e6fe4a71c2e624483b838adaf
AlekseyAleksandrov <axcerz@gmail.com>
2025-04-19 14:50:01 +0300

    add: redirect on Auth

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -2,10 +2,9 @@ import { useState, useEffect } from "react";
 import authReq from "../services/apiAuth";
 
 import Form from "../components/Form/From";
-import MainPage from '../pages/MainPage';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import QuestionPage from '../pages/QuestionPage';
-
+import MainPage from "../pages/MainPage";
+import { Route, Routes, Navigate } from "react-router-dom";
+import QuestionPage from "../pages/QuestionPage";
 
 function App() {
   const [userData, setUserData] = useState(null);
@@ -29,14 +28,25 @@ function App() {
     fetchUserData();
   }, []);
 
-  return (  <Routes>
-    <Route path="/" element={<Navigate to={!isAuth ? "/auth" : "/quiz"} />} />
-     <Route path="/auth" element={<Form setAuth={setAuth} /> } />
-    <Route path="/quiz" element={<MainPage />} />
-    <Route path="/quiz/:id" element={<QuestionPage />} />
-  </Routes>)
+  return (
+    <Routes>
+      {isAuth ? (
+        <>
+          <Route
+            path="/quiz"
+            element={isAuth ? <MainPage /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/quiz/:id"
+            element={isAuth ? <QuestionPage /> : <Navigate to="/auth" />}
+          />
+        </>
+      ) : (
+        <Route path="/auth" element={<Form setAuth={setAuth} />} />
+      )}
+      <Route path="*" element={<Navigate to={isAuth ? "/quiz" : "/auth"} />} />
+    </Routes>
+  );
 }
-// return <>{!isAuth ? <Form setAuth={setAuth} /> : <MainPage />}</>;
-
 
 export default App;
