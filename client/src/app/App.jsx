b0d1120ddea0e26d4f3d9aef12ea07160f3277de
Faceless Friend <pelk@ya.ru>
2025-04-19 15:32:38 +0300

    1

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -5,6 +5,7 @@ import Form from "../components/Form/From";
 import MainPage from '../pages/MainPage';
 import { Route, Routes } from 'react-router-dom';
 import QuestionPage from '../pages/QuestionPage';
+import PointPage from "../pages/PointsPage";
 
 function App() {
   const [userData, setUserData] = useState(null);
@@ -31,6 +32,7 @@ function App() {
   return (  <Routes>
     <Route path="/quiz" element={<MainPage />} />
     <Route path="/quiz/:id" element={<QuestionPage />} />
+    <Route path="/point/:id" element={<PointPage />} />
   </Routes>)
 }
 // return <>{!isAuth ? <Form setAuth={setAuth} /> : <MainPage />}</>;
---
+++ client/src/pages/PointsPage.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import $api from '../http/axiosConfig'
+
+function PointPage(){
+    const [points, setPoints] = useState([]);
+    const { id } = useParams();
+
+    useEffect(() => {
+        const fetchPoints = async () => {
+            try {
+                const userId = id ;
+
+                const response = await $api.get(`/points/${userId}`);
+                setPoints(response.data);
+            } catch (err) {
+                console.log(err)
+            } 
+        };
+
+        fetchPoints();
+    }, [id]);
+
+    console.log(points)
+
+    return (
+        <div>
+            <h2>Points for User {id}</h2>
+            <ul>
+                {points.map((point, index) => (
+                    <li key={index}>
+                        <p>`$`</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default PointPage
